d6eea15ea923f085519677e93fd15d60bfe86aa7
DaniilSmirnov <littlebigcrafting@gmail.com>
2019-01-25 06:05:17 +0300

    ъъъ

--- main.py
+++ main.py
@@ -998,7 +998,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupadminisUi()
+    ui.setupLoginUi()
     MainWindow.show()
     sys.exit(app.exec_())
 
